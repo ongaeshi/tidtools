2fe8daf6130996b28c7b9450c9f47a0e232c3c35
ongaeshi <ongaeshi@example.com>
2010-04-25 16:47:07 +0900

    match_line, match_only_titleを実装

--- lib/tidgrep/cli.rb
+++ lib/tidgrep/cli.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require File.join(File.dirname(__FILE__), '../tidtools/tiddle')
 require 'optparse'
 
@@ -8,8 +9,26 @@ module Tidgrep
       @title = nil
       @regexp_option = 0
       @report = false
-      @match_rule = "grep"
-      @keyword = nil
+      @match_rule = "line"
+    end
+
+    def setupParam(stdout, arguments)
+      opt = OptionParser.new('tidgrep [option] keyword')
+      opt.on('-f FILE_NAME', '--filename FILE_NAME', 'TiddlyWiki file name') {|v| @file_name = v }
+      opt.on('-t TITLE', '--title TITLE', 'match title') {|v| @title = v }
+      opt.on('-i', '--ignore', 'ignore case') {|v| @regexp_option |= Regexp::IGNORECASE }
+      opt.on('-r', '--report', 'disp report') {|v| @report = true }
+      opt.on('-m MATCH_RULE', '--match MATCH_RULE', 'match rule [line, tiddle, hr]') {|v| @match_rule = v }
+      opt.parse!(arguments)
+      
+      @keyword = arguments[0]
+      @title_regexp = @title && Regexp.new(@title, @regexp_option)
+      @content_regexp = @keyword && Regexp.new(@keyword, @regexp_option)
+
+      unless validOption?
+        puts opt.help
+        exit
+      end
     end
 
     def validOption?
@@ -17,83 +36,84 @@ module Tidgrep
       return @title || @keyword
     end
 
-    def execute(stdout, arguments=[])
-      opt = OptionParser.new('tidgrep [option] keyword')
-      opt.on('-f FILE_NAME', '--filename FILE_NAME', 'TiddlyWiki file name') {|v| @file_name = v }
-      opt.on('-t TITLE', '--title TITLE', 'match title') {|v| @title = v }
-      opt.on('-i', '--ignore', 'ignore case') {|v| @regexp_option |= Regexp::IGNORECASE }
-      opt.on('-r', '--report', 'disp report') {|v| @report = true }
-      opt.on('-m MATCH_RULE', '--match MATCH_RULE', 'match rule [grep, tiddle, hr]') {|v| @match_rule = v }
-      opt.parse!(arguments)
-
-      @keyword = arguments[0]
-
-      unless validOption?
-        puts opt.help
-        exit
-      end
-
+    def match_line
       tiddles = Tiddle.parse_sort_modified(@file_name)
+
       match_lines = 0
       total_lines = 0
       match_tiddles = 0
 
-      title_regexp = @title && Regexp.new(@title, @regexp_option)
-      content_regexp = @keyword && Regexp.new(@keyword, @regexp_option)
-
       tiddles.each do |tiddle|
-        next if (@title && tiddle.title !~ title_regexp)
+        next if (@title && tiddle.title !~ @title_regexp)
         is_match_tiddle = false
         line_no = 1
 
-        if (content_regexp)
-          tiddle.content.each_line do |line|
-#          tiddle.content.split(/----/).each do |line|
-            if (content_regexp =~ line)
-              case @match_rule
-              when "grep"
-                puts "#{tiddle.title}:#{line_no}:#{line}"
-#                print "#{line}"               
-#                print "----"
-                match_lines += 1
-                unless is_match_tiddle
-                  match_tiddles += 1
-                  is_match_tiddle = true
-                end
-              when "tiddle"
-                match_lines += 1
-                unless is_match_tiddle
-                  puts "--- #{tiddle.title} --------------------"
-                  match_tiddles += 1
-                  is_match_tiddle = true
-                end
-                puts "#{line}"
-              when "hr"
-                print "#{line}"               
-                print "----"
-              end
+        tiddle.content.each_line do |line|
+          if (@content_regexp =~ line)
+            puts "#{tiddle.title}:#{line_no}:#{line}"
+            match_lines += 1
+            unless is_match_tiddle
+              match_tiddles += 1
+              is_match_tiddle = true
             end
-            line_no += 1
-            total_lines += 1
           end
-        else
-          puts tiddle.title
-          match_tiddles += 1
-          is_match_tiddle = true
+          line_no += 1
+          total_lines += 1
         end
-
       end
 
       if (@report)
         puts "------------------------------"
-        if (content_regexp)
-          puts "match lines : #{match_lines}"
-          puts "total lines : #{total_lines}"
-        end
-        puts "match tiddles : #{match_tiddles}"
+        puts "search lines : #{total_lines}"
+        puts "match lines : #{match_lines}"
         puts "total tiddles : #{tiddles.size}"
+        puts "match tiddles : #{match_tiddles}"
       end
     end
+
+    def match_only_title
+      tiddles = Tiddle.parse_sort_modified(@file_name)
+
+      match_tiddles = 0
+
+      tiddles.each do |tiddle|
+        next if (@title && tiddle.title !~ @title_regexp)
+        puts tiddle.title
+        match_tiddles += 1
+      end
+
+      if (@report)
+        puts "------------------------------"
+        puts "total tiddles : #{tiddles.size}"
+        puts "match tiddles : #{match_tiddles}"
+      end
+    end
+
+    def match_tiddle
+    end
+
+    def match_hr
+    end
+
+    def execute(stdout, arguments=[])
+      # パラメータの設定
+      setupParam(stdout, arguments)
+      
+      # マッチルールごとに処理を変える
+      if (@content_regexp) 
+        case @match_rule
+        when "line"
+          match_line
+        when "tiddle"
+          match_tiddle
+        when "hr"
+          match_hr
+        end
+      else
+        match_only_title
+      end
+    end
+
   end
 
   class CLI
